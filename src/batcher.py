3b96b5dfb73bafef84061143658c8dfea8e8d786
Jacek Jankowiak <jac.jankow@gmail.com>
2020-05-25 21:28:25 +0000

    ready batcher, to be tested

--- src/batcher.py
+++ src/batcher.py
@@ -7,24 +7,33 @@ class Batcher:
         self.batch_size = BATCH_SIZE
         self.buffer_size = BUFFER_SIZE
         self.data_dir_prefix = data_dir_prefix
-        self.all_steps = []
+        self.buffer = []
         self.batch = []
         self.known_csvs = []
-        self.batch_iterator = BATCH_SIZE
+        self.current_step_idx = 0
         random.seed(random_seed)
         
         #initially explore the data system in search of already generated data
         for filename in glob.iglob(self.data_dir_prefix+'**/*.csv', recursive=True):
             self.known_csvs.append(filename)
         
-        for csv_file in self.known_csvs:
-            df = pd.read_csv(csv_file)
-            steps = df['step'].tolist()
-            for step in steps:
-                self.all_steps.append(csv_file+"-"+step)
-                
-        self.buffer = self.all_steps[0:BATCH_SIZE]
-                
+        #problemy:
+        #1. ładuję dane i jest poniżej buffersize
+        #2. ładuję dane i kończę w połowie pliku
+        #3. ładuję dane i kończę idealnie na końcu pliku
+        
+        self.current_csv_idx = 0
+        df = pd.read_csv(self.known_csvs[self.current_csv_idx])
+        self.current_csv_steps = df['step'].tolist()
+        
+        
+        while len(self.buffer) < BUFFER_SIZE:
+            try:
+                self.append_buffer()
+            except:
+                #should get here if we try to open a csv when we run out of them
+                print("Ran out of data to read while preloading data buffer")
+     
     def __fill_batch():
         self.batch = []
         for i in range self.batch_size:
@@ -32,11 +41,21 @@ class Batcher:
             step = Observation(drawn_step[0], drawn_step[1])
             self.batch.append(step)
 
+    def append_buffer():
+        self.buffer.append(self.known_csvs[self.current_csv_idx] + "-" + self.current_step_idx)
+        self.current_step_idx += 1
+        if len(self.buffer) > BUFFER_SIZE:
+            self.buffer.remove(self.buffer[0])
+        if self.current_step_idx == len(self.current_csv_steps):
+            self.current_csv_idx += 1
+            df = pd.read_csv(self.known_csvs[self.current_csv_idx])
+            self.current_csv_steps = df['step'].tolist()
+            self.current_step_idx = 0
+        
+    
     def get_batch(new_step_csv, new_step_idx):
         self.__fill_batch()
-        self.all_steps.append(new_step_csv+"-"+new_step_idx)
-        self.buffer.remove(self.buffer[0])
-        self.buffer.append(self.all_steps[self.batch_iterator])
+        self.append_buffer()
         return self.batch
     
     
